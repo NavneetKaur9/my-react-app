978754470734adbd6612043b9d07e71088a2a4e2
NavneetKaur9 <pannu.navneet9@gmail.com>
2019-12-04 01:25:52 +0530

    hooks

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,35 @@
-import React from 'react';
-import { User } from './Users';
-import './App.scss';
+import React, { useState, useEffect } from 'react';
 
-class App extends React.Component {
+function App() {
+    // Declare a new state variable, which we'll call "count"
+    const [count, setCount] = useState(0);
+    // Declare multiple state variables!
+    const [age, setAge] = useState(42);
+    const [fruit, setFruit] = useState('banana');
+    const [todos, setTodos] = useState([{ text: 'Learn Hooks' }]);
 
-    render() {
-        return (
-            <div className="App">
-                <User />
-            </div>
-        );
-    }
+    // Similar to componentDidMount and componentDidUpdate:
+    useEffect(() => {
+        // Update the document title using the browser API
+        document.title = `You clicked ${count} times`;
+    });
+
+    useEffect(() => {
+        return () => console.log("after effect", count);
+    });
+
+    return (
+        <div>
+            <p>You clicked {count} times</p>
+            <button onClick={() => setCount(count + 1)}>
+                Click me
+            </button>
+            <div> Age: {age} </div>
+            <div> Fruit: {fruit} </div>
+            <div> Todos: {todos.map(todo => (<div >{todo.text}</div>))} </div>
+
+        </div>
+    );
 }
 
 export default App;
